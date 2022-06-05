619549041cba6a1db2ffc0190980e95a97dff2a9
habibmustafa <hebibullahmustafazade@gmail.com>
2022-06-05 14:28:32 +0400

    gifs favorite add and remove

--- src/Components/Favorites.js
+++ src/Components/Favorites.js
@@ -1,10 +1,31 @@
 import React from 'react'
 import './Favorites.css'
+import Masonry from 'react-masonry-css'
+import GifItem from './GifItem'
 
 const Favorites = () => {
+
+   let favorites = JSON.parse(localStorage.favoriteItem)
+
+   const breakPoints = {
+      default: 4,
+      1170: 3,
+      840: 2,
+      600: 1
+   }
+
    return (
       <div className="favorites">
          <h2>Favorites</h2>
+         <Masonry
+               breakpointCols={breakPoints}
+               className="my-masonry-grid"
+               columnClassName="my-masonry-grid_column"
+            >
+               {favorites.map(item => (
+                  <GifItem key={item.id} {...item} />
+               )) }
+            </Masonry>
       </div>
    )
 }
--- src/Components/GifItem.js
+++ src/Components/GifItem.js
@@ -4,13 +4,37 @@ import { useNavigate } from 'react-router-dom'
 
 function GifItem({ ...item }) {
    const [hover, setHover] = useState(false)
-   const [heartActive, setHeartActive] = useState(false)
 
    const heartRef = useRef()
 
    let navigate = useNavigate()
    const handleClick = (e) => {
       heartRef.current.contains(e.target) || navigate(`/view/${item.id}`)
+
+   }
+
+   let favoriteItemArray = [];
+   !localStorage.favoriteItem && localStorage.setItem('favoriteItem', JSON.stringify(favoriteItemArray))
+
+   favoriteItemArray = JSON.parse(localStorage.favoriteItem)
+
+   let check = (favoriteItemArray.find(a=> a.id == item.id) === undefined ? false : favoriteItemArray.find(a=> a.id === item.id).id === item.id)
+
+   const [heartActive, setHeartActive] = useState(check)
+
+
+   const heartClick = () => {
+      if (!Boolean(favoriteItemArray.find(a=> a.id === item.id))) {
+         favoriteItemArray.push({...item, status: !heartActive})
+         localStorage.setItem('favoriteItem', JSON.stringify(favoriteItemArray))
+         setHeartActive(true)
+      }
+      else {
+         const index = favoriteItemArray.findIndex(a => a.id === item.id)
+         favoriteItemArray.splice(index, 1)
+         localStorage.setItem('favoriteItem', JSON.stringify(favoriteItemArray))
+         setHeartActive(false)
+      }
    }
 
    return (
@@ -18,16 +42,19 @@ function GifItem({ ...item }) {
          onMouseEnter={() => setHover(true)}
          onMouseLeave={() => setHover(false)}
          onClick={(e) => handleClick(e)}
-
          className="gif" style={{ height: `${item.media[0].tinygif.dims[1]}px` }}
       >
          {hover &&
             <div
                ref={heartRef}
-               onClick={() => setHeartActive(!heartActive)}
+               onClick={heartClick}
                className="heart-box"
             >
-               <span tabIndex={30} className={`heart ${heartActive && 'heart-active'}`}></span>
+               <span
+                  tabIndex={30}
+                  className={`${heartActive ? 'heart heart-active' : 'heart'}`}>
+
+               </span>
             </div>}
          <img className='item-img' src={item.media[0].tinygif.url} alt={item.content_description} />
       </div>
